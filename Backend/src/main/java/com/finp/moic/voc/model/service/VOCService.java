db6ee6e3969b43238c9c81121aeb276d374168b8
ssjjaa-algo <tjdwo6269@naver.com>
2023-12-15 14:41:46 +0900

    Refactor: voc 패키지 삭제

--- Backend/src/main/java/com/finp/moic/voc/model/service/VOCService.java
+++
@@ -1,7 +0,0 @@
-package com.finp.moic.voc.model.service;
-
-import com.finp.moic.voc.model.dto.request.VOCRequestDTO;
-
-public interface VOCService {
-    void sendVOC(String id, VOCRequestDTO dto);
-}
