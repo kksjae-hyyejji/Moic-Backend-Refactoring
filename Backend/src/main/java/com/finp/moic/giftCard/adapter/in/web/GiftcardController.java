fff7b713107d9144fa2e026bbfe314458d39ec27
ssjjaa-algo <tjdwo6269@naver.com>
2023-12-18 15:16:17 +0900

    Refactor: GiftcardController 의존성 주입

--- Backend/src/main/java/com/finp/moic/giftCard/adapter/in/web/GiftcardController.java
+++ Backend/src/main/java/com/finp/moic/giftCard/adapter/in/web/GiftcardController.java
@@ -1,12 +1,15 @@
 package com.finp.moic.giftCard.adapter.in.web;
 
 import com.finp.moic.giftCard.adapter.in.request.GiftcardDeleteRequestDTO;
-import com.finp.moic.giftCard.application.port.in.GiftcardUseCase;
+import com.finp.moic.giftCard.application.RegistGiftcardService;
+import com.finp.moic.giftCard.application.port.in.DeleteGiftcardUseCase;
+import com.finp.moic.giftCard.application.port.in.GetMyGiftcardUseCase;
 import com.finp.moic.giftCard.application.response.GiftcardListServiceResponse;
 import com.finp.moic.giftCard.adapter.out.persistence.ImageS3Adapter;
 import com.finp.moic.util.dto.ResponseDTO;
 import com.finp.moic.user.security.dto.UserAuthentication;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,16 +22,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/gift")
+@RequiredArgsConstructor
 public class GiftcardController {
 
-    private final GiftcardUseCase giftcardUseCase;
-    private final ImageS3Adapter commandImageS3Port;
-
-    @Autowired
-    public GiftcardController(GiftcardUseCase giftcardUseCase, ImageS3Adapter commandImageS3Port) {
-        this.giftcardUseCase = giftcardUseCase;
-        this.commandImageS3Port = commandImageS3Port;
-    }
+    private final DeleteGiftcardUseCase deleteGiftcardUseCase;
+    private final GetMyGiftcardUseCase getMyGiftcardUseCase;
+    private final RegistGiftcardService registGiftcardService;
 
     @PostMapping("/regist")
     @Transactional
@@ -37,7 +36,7 @@ public class GiftcardController {
                                                 @AuthenticationPrincipal UserAuthentication userAuthentication
     ,HttpServletRequest httpServletRequest) {
 
-        giftcardUseCase.regist(userAuthentication.getId(),multipartFile);
+        registGiftcardService.regist(userAuthentication.getId(),multipartFile);
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDTO.builder()
                 .message("등록이 완료되었습니다.")
@@ -48,7 +47,7 @@ public class GiftcardController {
     public ResponseEntity<ResponseDTO> delete(@RequestBody GiftcardDeleteRequestDTO giftcardDeleteRequestDTO,
                                               @AuthenticationPrincipal UserAuthentication userAuthentication) {
 
-        giftcardUseCase.delete(userAuthentication.getId(),giftcardDeleteRequestDTO.getImageUrl());
+        deleteGiftcardUseCase.delete(userAuthentication.getId(),giftcardDeleteRequestDTO.getImageUrl());
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDTO.builder()
                 .message("삭제가 완료되었습니다.")
@@ -58,7 +57,7 @@ public class GiftcardController {
     @GetMapping("/mygifts")
     public ResponseEntity<ResponseDTO> mygifts(@AuthenticationPrincipal UserAuthentication userAuthentication) {
 
-        List<GiftcardListServiceResponse> list = giftcardUseCase.mygifts(userAuthentication.getId());
+        List<GiftcardListServiceResponse> list = getMyGiftcardUseCase.mygifts(userAuthentication.getId());
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDTO.builder()
                 .message("내 기프티콘 목록 조회")
